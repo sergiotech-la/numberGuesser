b1435a5fb06654620a6c639dc38add3ac2c4aee4
Sergio Herrera <sa.herrerasaavedra@gmail.com>
2023-05-25 12:15:53 -0500

    Fix generateTarget() and compareGuesses()

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ let currentRoundNumber = 1;
 // Write your code below:
 
 function generateTarget() {
-    return Math.floor(Math.random * 9);
+    return Math.floor(Math.random() * 10);
 }
 
 //  If both players are tied, the human user should win. Return true if the human player wins, and false if the computer player wins.
 
 function compareGuesses(human, computer, number) {
-    human === computer ? true : Math.abs(human - number) < Math.abs(computer - number);
+    return human == computer ? true : Math.abs(human - number) <= Math.abs(computer - number);
 }
 
 function updateScore(winner) {
-    winner === 'human' ? humanScore += 1 : computerScore += 1;
+    winner == 'human' ? humanScore += 1 : computerScore += 1;
 }
 
 function advanceRound() {
